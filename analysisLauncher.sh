ed2b883b7f3a98018b0bfa0fce84f759f802b0f7
Sam Rendall <sdrendall@gmail.com>
2016-12-13 15:35:14 -0500

    Changed input path for analysisLauncher.sh

--- analysisLauncher.sh
+++ analysisLauncher.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-for video in `find ~/sr235/recallVideos -type f -name "*.mp4"`
+for video in `find /groups/gray/sam/Big_experiment_video -type f -name "*.mp4"`
 do
     logName=$(basename $video .mp4)
     bsub -q short -R "rusage[mem=32000]" -W 3:00 -e ~/jobLogs/$logName.err -o ~/jobLogs/$logName.log matlab -nosplash -nojvm -nodesktop -r "quantifyMovement_video('$video')"
