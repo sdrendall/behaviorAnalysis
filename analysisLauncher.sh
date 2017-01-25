7b0c540909bddb59da20e5710bf5e8d65fb2720c
Sam Rendall <sdrendall@gmail.com>
2017-01-25 11:21:29 -0500

    Changed the search path for analysisLauncher to reflect new video location and filetype

--- analysisLauncher.sh
+++ analysisLauncher.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-for video in `find /groups/gray/sam/Big_experiment_video -type f -name "*.mp4"`
+for video in `find /groups/gray/sam/Big_experiment_video -type f -name "*.avi"`
 do
-    logName=$(basename $video .mp4)
-    bsub -q short -R "rusage[mem=32000]" -W 3:00 -e ~/jobLogs/$logName.err -o ~/jobLogs/$logName.log matlab -nosplash -nojvm -nodesktop -r "quantifyMovement_video('$video')"
+    logName=$(basename $video .avi)
+    bsub -q short -R "rusage[mem=8000]" -W 8:00 -e ~/jobLogs/$logName.err -o ~/jobLogs/$logName.log matlab -nosplash -nojvm -nodesktop -r "quantifyMovement_video('$video')"
 done
